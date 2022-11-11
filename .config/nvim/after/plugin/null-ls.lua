66c4f7dc480768a4695671e0db5b01fbcfe789fa
Julio Vizcaíno <juliojesusvizcaino@gmail.com>
2022-11-11 17:50:57 +0100

    Null-ls changes

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -1,16 +1,17 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.diagnostics.shellcheck,
-        -- null.builtins.completion.spell, -- too many text results
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.shellharden,
-        null_ls.builtins.code_actions.gitsigns,
-        null_ls.builtins.code_actions.eslint_d,
-        null_ls.builtins.code_actions.shellcheck,
-    },
+	sources = {
+		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.diagnostics.shellcheck,
+		null_ls.builtins.diagnostics.mypy,
+		-- null.builtins.completion.spell, -- too many text results
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.isort,
+		null_ls.builtins.formatting.shellharden,
+		null_ls.builtins.code_actions.gitsigns,
+		null_ls.builtins.code_actions.eslint_d,
+		null_ls.builtins.code_actions.shellcheck,
+	},
 })
