a2e14def2c19c025ff4ee37658b3c016098a8f81
Julio Vizcaíno <juliojesusvizcaino@gmail.com>
2022-12-20 18:27:33 +0100

    Fix tabs

--- .config/nvim/after/plugin/default.lua
+++ .config/nvim/after/plugin/default.lua
@@ -9,3 +9,8 @@ local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "floa
 
 vim.keymap.set('n', "<leader>`", "<C-^>", { desc = 'Change to previous buffer' })
 vim.keymap.set('n', "<leader>gg", function() lazygit:toggle() end, { desc = 'lazygit' })
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
