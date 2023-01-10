0eb8e0fc03aed2737300f1e48b9268cb43c1aa29
Julio Vizcaíno <juliojesusvizcaino@gmail.com>
2023-01-10 14:03:46 +0100

    Migrate to lazy nvim

--- .config/nvim/lua/plugins/additional.lua
+++ .config/nvim/lua/plugins/additional.lua
@@ -1,69 +1,69 @@
-return function(use)
-  use({
+return {
+  {
     "folke/which-key.nvim",
     config = function()
       require("which-key").setup({})
     end,
-  })
-  use({
+  },
+  {
     "rcarriga/nvim-notify",
     config = function()
       require("notify").setup({
         background_colour = "#000000",
       })
     end,
-  })
-  use({
+  },
+  {
     "kylechui/nvim-surround",
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
     config = function()
       require("nvim-surround").setup({
         -- Configuration here, or leave empty to use defaults
       })
     end,
-  })
-  use({ "nvim-telescope/telescope-file-browser.nvim" })
-  use({ "ThePrimeagen/harpoon", requires = "nvim-lua/plenary.nvim" })
-  use({
+  },
+  { "nvim-telescope/telescope-file-browser.nvim" },
+  { "ThePrimeagen/harpoon", dependencies = "nvim-lua/plenary.nvim" },
+  {
     "akinsho/toggleterm.nvim",
-    tag = "*",
+    version = "*",
     config = function()
       require("toggleterm").setup({
         open_mapping = "<a-space>",
       })
     end,
-  })
-  use({
+  },
+  {
     "AckslD/nvim-pytrize.lua",
     config = function()
       require("pytrize").setup()
     end,
-  })
+  },
 
-  use("ThePrimeagen/vim-be-good")
-  use("jose-elias-alvarez/typescript.nvim")
-  use({
+  "ThePrimeagen/vim-be-good",
+  "jose-elias-alvarez/typescript.nvim",
+  {
     "nvim-telescope/telescope-frecency.nvim",
     config = function()
       require("telescope").load_extension("frecency")
     end,
-    requires = { "kkharji/sqlite.lua" },
-  })
-  use("jose-elias-alvarez/null-ls.nvim")
-  use({
+    dependencies = { "kkharji/sqlite.lua" },
+  },
+  "jose-elias-alvarez/null-ls.nvim",
+  {
     "iamcco/markdown-preview.nvim",
-    run = function()
+    build = function()
       vim.fn["mkdp#util#install"]()
     end,
-  })
-  use({ "knubie/vim-kitty-navigator", run = "cp ./*.py ~/.config/kitty/" })
-  use({
+  },
+  { "knubie/vim-kitty-navigator", build = "cp ./*.py ~/.config/kitty/" },
+  {
     "klen/nvim-test",
     config = function()
       require("nvim-test").setup({
         term = "toggleterm",
       })
     end,
-  })
-  use("emmanueltouzery/agitator.nvim")
-end
+  },
+  "emmanueltouzery/agitator.nvim",
+}
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,99 @@
+return {
+	{ -- LSP Configuration & Plugins
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+
+			-- Useful status updates for LSP
+			"j-hui/fidget.nvim",
+		},
+	},
+
+	{ -- Autocompletion
+		"hrsh7th/nvim-cmp",
+		dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
+	},
+
+	{ -- Highlight, edit, and navigate code
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+		end,
+		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+		-- -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
+		-- local has_plugins, plugins = pcall(require, "custom.plugins")
+		-- if has_plugins then
+		--         plugins(use)
+		-- end
+	},
+
+	-- Git related plugins
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
+	{
+		"lewis6991/gitsigns.nvim",
+		config = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+
+	"navarasu/onedark.nvim", -- Theme inspired by Atom
+	"shaunsingh/nord.nvim",
+	"drewtempelmeyer/palenight.vim",
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+
+		config = function()
+			require("tokyonight").setup({
+				transparent = true,
+				styles = {
+					sidebars = "transparent",
+				},
+			})
+
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		config = {
+			options = {
+				icons_enabled = true,
+				theme = "tokyonight",
+				component_separators = "|",
+				section_separators = "",
+			},
+		},
+	}, -- Fancier statusline
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = {
+			char = "┊",
+			show_trailing_blankline_indent = false,
+		},
+	}, -- Add indentation guides even on blank lines
+
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	}, -- "gc" to comment visual regions/lines
+	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+
+	-- Fuzzy Finder (files, lsp, etc)
+	{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+
+	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+}
