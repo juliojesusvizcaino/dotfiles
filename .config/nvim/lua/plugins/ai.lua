71dcd3f82a4cb337ffdd9f3423ed501dba4228d6
Julio Vizcaíno <juliojesusvizcaino@gmail.com>
2024-04-30 17:04:13 +0200

    AIiiii

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,55 +1,70 @@
 ---@type LazyConfig
 return {
-  -- {
-  --   "Exafunction/codeium.nvim",
-  --   opts = {
-  --     enable_chat = true,
-  --     enable_local_search = true,
-  --     enable_index_service = true,
-  --   },
-  -- },
+  {
+    "Exafunction/codeium.nvim",
+    opts = {
+      enable_chat = true,
+      -- enable_local_search = true,
+      -- enable_index_service = true,
+      -- tools = {
+      --   language_server = "/home/julio/.local/lib/language_server_linux_x64",
+      -- },
+    },
+    keys = {
+      { "<leader>cx", "<cmd>Codeium Chat<cr>", desc = "Codeium - Chat", mode = { "n", "v" } },
+    },
+    cmd = { "Codeium" },
+    lazy = false,
+  },
   -- {
   --   "Exafunction/codeium.vim",
   --   event = "BufEnter",
+  --   -- disabled in options.lua
+  --   -- setup = function()
+  --   --   vim.g.codeium_enabled = false
+  --   -- end,
+  --   keys = {
+  --     { "<leader>cx", "<cmd>call codeium#Chat()<cr>", desc = "Codeium - Chat", mode = { "n", "v" } },
+  --   },
   -- },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      "zbirenbaum/copilot.lua", -- or github/copilot.vim
-      "nvim-lua/plenary.nvim", -- for curl, log wrapper
-      "nvim-telescope/telescope.nvim",
-    },
-    opts = {
-      -- debug = true, -- Enable debugging
-      -- See Configuration section for rest
-      context = "buffers",
-    },
-    lazy = false,
-    -- See Commands section for default commands if you want to lazy load on them
-    keys = {
-      -- Show help actions with telescope
-      {
-        "<leader>czh",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.telescope").pick(actions.help_actions())
-        end,
-        desc = "CopilotChat - Help actions",
-        mode = { "n", "v" },
-      },
-      -- Show prompts actions with telescope
-      {
-        "<leader>czp",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
-        end,
-        desc = "CopilotChat - Prompt actions",
-        mode = { "n", "v" },
-      },
-    },
-  },
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   branch = "canary",
+  --   dependencies = {
+  --     "zbirenbaum/copilot.lua", -- or github/copilot.vim
+  --     "nvim-lua/plenary.nvim", -- for curl, log wrapper
+  --     "nvim-telescope/telescope.nvim",
+  --   },
+  --   opts = {
+  --     -- debug = true, -- Enable debugging
+  --     -- See Configuration section for rest
+  --     context = "buffers",
+  --   },
+  --   lazy = false,
+  --   -- See Commands section for default commands if you want to lazy load on them
+  --   keys = {
+  --     -- Show help actions with telescope
+  --     {
+  --       "<leader>czh",
+  --       function()
+  --         local actions = require("CopilotChat.actions")
+  --         require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+  --       end,
+  --       desc = "CopilotChat - Help actions",
+  --       mode = { "n", "v" },
+  --     },
+  --     -- Show prompts actions with telescope
+  --     {
+  --       "<leader>czp",
+  --       function()
+  --         local actions = require("CopilotChat.actions")
+  --         require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+  --       end,
+  --       desc = "CopilotChat - Prompt actions",
+  --       mode = { "n", "v" },
+  --     },
+  --   },
+  -- },
   -- You exceeded your current quota :shrug:
   --   you need to pay
   -- {
