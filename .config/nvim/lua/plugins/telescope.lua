db391187cde7f216d3d72e38192377b4df7b8105
Julio Vizcaíno <juliojesusvizcaino@gmail.com>
2024-06-11 16:53:22 +0200

    Better filenames

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,14 +4,25 @@ return {
     "nvim-telescope/telescope.nvim",
     opts = {
       defaults = {
-        path_display = { "smart" },
+        -- path_display = { "filename_first" },
       },
       pickers = {
         live_grep = {
           additional_args = { "--hidden" },
+          -- path_display = { "smart" },
         },
         grep_string = {
           additional_args = { "--hidden" },
+          -- path_display = { "smart" },
+        },
+        find_files = {
+          path_display = {
+            "filename_first",
+            -- shorten = 3,
+          },
+        },
+        git_files = {
+          -- path_display = { "filename_first" },
         },
       },
     },
@@ -22,13 +33,13 @@ return {
       -- { "<leader>hk", require("telescope.builtin").keymaps, desc = "[S]earch [H]elp" },
     },
     dependencies = {
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-        config = function()
-          require("telescope").load_extension("fzf")
-        end,
-      },
+      -- {
+      --   "nvim-telescope/telescope-fzf-native.nvim",
+      --   build = "make",
+      --   config = function()
+      --     require("telescope").load_extension("fzf")
+      --   end,
+      -- },
       --   Error  10:55:36 msg_show.emsg E5108: Error executing lua: ...escope-all-recent.nvim/lua/telescope-all-recent/init.lua:35: attempt to index local 'select_opts' (a nil value)
       {
         "prochri/telescope-all-recent.nvim",
