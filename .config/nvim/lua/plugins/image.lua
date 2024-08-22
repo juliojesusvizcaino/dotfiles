0aedb15f16a318089cf06202ea862538ba09f6f3
Julio Vizcaíno <juliojesusvizcaino@gmail.com>
2024-08-22 09:27:03 +0200

    Add images support to nvim

---
+++ .config/nvim/lua/plugins/image.lua
@@ -0,0 +1,19 @@
+---@type LazyConfig
+return {
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1001, -- this plugin needs to run before anything else
+    opts = {
+      rocks = { "magick" },
+    },
+  },
+  {
+    "3rd/image.nvim",
+    dependencies = { "luarocks.nvim" },
+    opts = {
+      integrations = {
+        html = { enabled = true },
+      },
+    },
+  },
+}
