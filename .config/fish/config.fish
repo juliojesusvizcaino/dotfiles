e2a83c6324ea05eb73fad47c9ba77e422e3534fd
Julio Vizcaíno <juliojesusvizcaino@gmail.com>
2024-11-11 09:37:45 +0100

    Conda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,3 +16,17 @@ fish_add_path ~/.local/share/mise/shims
 ~/.local/bin/mise activate fish | source
 
 fzf --fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/julio/miniconda3/bin/conda
+    eval /home/julio/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/julio/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/julio/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/julio/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
