cc02768fdda865e1840bd8548d47d24abe5cf551
Julio Vizcaíno <juliojesusvizcaino@gmail.com>
2024-11-12 13:29:06 +0100

    karapace

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,3 +30,10 @@ else
 end
 # <<< conda initialize <<<
 
+
+if test -f /usr/bin/carapace
+  set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
+  mkdir -p ~/.config/fish/completions
+  carapace --list | awk '{print $1}' | xargs -I{} touch ~/.config/fish/completions/{}.fish # disable auto-loaded completions (#185)
+  carapace _carapace | source
+end
